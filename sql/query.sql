fc215497912b4e2b4e3d131a703e1a53432edde1
long <longnguyen176251@gmail.com>
2025-02-08 09:42:06 -0600

    got sqlc codegen working

--- sql/query.sql
+++ sql/query.sql
@@ -2,22 +2,8 @@
 SELECT
   *
 FROM
-  db_events
+  event
 WHERE
   url = ?
 LIMIT
   1;
-
--- name: CreateEvent :exec
-INSERT INTO
-  db_events (url)
-VALUES
-  (?) ON CONFLICT (url) DO NOTHING;
-
--- name: UpdateEvent :exec
-UPDATE db_events
-set
-  data = ?,
-  history = ?
-WHERE
-  url = ?;
