293396ec86cf0590837a06bb39a12ed405ebbef9
long <longnguyen176251@gmail.com>
2025-02-23 20:40:38 -0600

    - added default time for timestamp in UI - added environment variable to set current time

--- src/server.go
+++ src/server.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"embed"
+	"fmt"
 	"io/fs"
 	"mmaschedule-go/event"
 	"net/http"
+	"os"
+	"strconv"
 	"time"
 )
 
@@ -37,7 +40,7 @@ func StaticHandler(prefix string) (http.Handler, error) {
 }
 
 func RouteIndex(w http.ResponseWriter, r *http.Request, q *event.Queries) {
-	slug, err := q.GetUpcomingEvent(r.Context(), time.Now().Unix())
+	slug, err := q.GetUpcomingEvent(r.Context(), EventTime())
 	if err != nil {
 		http.NotFound(w, r)
 	} else {
@@ -59,13 +62,31 @@ func RouteEvent(w http.ResponseWriter, r *http.Request, q *event.Queries) {
 		return
 	}
 
-	upcoming, err := q.ListUpcomingEvents(r.Context(), time.Now().Unix())
+	upcoming, err := q.ListUpcomingEvents(r.Context(), EventTime())
 	if err != nil {
 		http.NotFound(w, r)
 		return
 	}
 
 	TemplEventPage(event, upcoming).Render(r.Context(), w)
+}
+
+func EventTime() int64 {
+	timestamp, err := EnvTime()
+	if err != nil {
+		t := time.Now()
+		return t.Add(time.Duration(-6) * time.Hour).Unix()
+	} else {
+		return timestamp
+	}
+}
+
+func EnvTime() (int64, error) {
+	timestr := os.Getenv("CURRENT_TIME")
+	if len(timestr) == 0 {
+		return 0, fmt.Errorf("Time env is empty.")
+	}
+	return strconv.ParseInt(timestr, 10, 64)
 }
 
 type StateHandler func(w http.ResponseWriter, r *http.Request, q *event.Queries)
